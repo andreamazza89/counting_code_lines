36bd4c8ec5705600e4992704ad49b3ce6d440d7f
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-11-09 12:15:57 +0000

    First implementation of multi-line comments removal

--- lib/lines_counter.rb
+++ lib/lines_counter.rb
@@ -3,7 +3,15 @@ class LinesCounter
   def initialize(arguments)
     source_code = arguments[:source_code]
     @source_lines = source_code.split("\n")
+
+########################maybe extract the instances below as a Rule class?######
     @one_line_regex = arguments[:one_line_regex] || JAVA_ONE_LINE_COMMENT_REGEX
+    @multiline_start_regex = arguments[:multiline_start_regex] || 
+                               JAVA_MULTILINE_COMMENT_START_REGEX
+
+    @multiline_end_regex = arguments[:multiline_end_regex] || 
+                               JAVA_MULTILINE_COMMENT_END_REGEX
+################################################################################
   end
 
   def self.lines_of_code(source_code)
@@ -12,14 +20,18 @@ class LinesCounter
 
   def lines_of_code
     return 0 if source_lines.empty?
+
     no_empty_lines = remove_empty_lines(source_lines)
     no_one_liners = remove_one_line_comments(no_empty_lines)
-    no_one_liners.count
+    no_multiline_no_one_line = remove_multiline_comments(no_one_liners)
+
+    no_multiline_no_one_line.count
   end
 
   private
   
-  attr_reader :source_lines, :one_line_regex
+  attr_reader :source_lines, :one_line_regex, 
+              :multiline_start_regex, :multiline_end_regex
 
   def remove_empty_lines(lines)
     lines.reject { |line| line =~ EMPTY_LINE_REGEX }
@@ -29,7 +41,20 @@ class LinesCounter
     lines.reject { |line| line =~ one_line_regex }
   end
 
+  def remove_multiline_comments(lines)
+    inside_comment = false
+    output = []
+    lines.each do |line|
+      inside_comment = true if line =~ multiline_start_regex
+      output << line unless inside_comment
+      inside_comment = false if line =~ multiline_end_regex
+    end
+    output
+  end
+
   JAVA_ONE_LINE_COMMENT_REGEX = /^\s*\/\//
   EMPTY_LINE_REGEX = /^\s*$/
+  JAVA_MULTILINE_COMMENT_START_REGEX = /^\/\*/
+  JAVA_MULTILINE_COMMENT_END_REGEX = /\*\//
 
 end
--- spec/unit/lines_counter_spec.rb
+++ spec/unit/lines_counter_spec.rb
@@ -1,32 +1,37 @@
 describe LinesCounter, '#lines_of_code' do
 
-  it 'returns 0 for an empty string' do
-    expect(described_class.lines_of_code("")).to eq 0
+  context 'default - java' do
+    it 'returns 0 for an empty string' do
+      expect(described_class.lines_of_code("")).to eq 0
+    end
+
+    it 'returns 1 for a string with a single line that is not a comment' do
+      expect(described_class.lines_of_code("private int number = 3;")).to eq 1
+    end
+
+  #At this point, I start thinking that it would make a lot of sense to use start/end
+  #comment anchors, should I let this shape future tests? or should future tests 
+  #make this emerge? Maybe I should pretend the Client is only asking for one language
+  #but keep an eye open for potential new languages...
+
+    it 'returns 0 for a string with a single line being a comment ' do
+      expect(described_class.lines_of_code("//private int number = 3;")).to eq 0
+    end
+
+    it 'returns 0 for a string with a single line being a comment, starts with space' do
+      expect(described_class.lines_of_code(" //private int number = 3;")).to eq 0
+    end
+
+    it 'ignores empty lines' do 
+      expect(described_class.lines_of_code(" \n private int number = 3;")).to eq 1
+    end
+
+    it 'ignores one-line-comment anchors if they are not at the start of the line' do 
+      expect(described_class.lines_of_code("private String link = //missing")).to eq 1
+    end
+
+    it 'ignores multi-line comments (example with no lines of code)' do
+      expect(described_class.lines_of_code("/* comment \n * com \n ent */")).to eq 0
+    end
   end
-
-  it 'returns 1 for a string with a single line that is not a comment' do
-    expect(described_class.lines_of_code("private int number = 3;")).to eq 1
-  end
-
-#At this point, I start thinking that it would make a lot of sense to use start/end
-#comment anchors, should I let this shape future tests? or should future tests 
-#make this emerge? Maybe I should pretend the Client is only asking for one language
-#but keep an eye open for potential new languages...
-
-  it 'returns 0 for a string with a single line being a comment (java)' do
-    expect(described_class.lines_of_code("//private int number = 3;")).to eq 0
-  end
-
-  it 'returns 0 for a string with a single line being a comment, starts with space (java)' do
-    expect(described_class.lines_of_code(" //private int number = 3;")).to eq 0
-  end
-
-  it 'ignores empty lines' do 
-    expect(described_class.lines_of_code(" \n private int number = 3;")).to eq 1
-  end
-
-  it 'ignores one-line-comment anchors if they are not at the start of the line' do 
-    expect(described_class.lines_of_code("private String link = //missing")).to eq 1
-  end
-
 end
