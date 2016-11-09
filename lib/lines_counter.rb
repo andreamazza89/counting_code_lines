550eceaf9ea88631469d181f0fc29bd239f4ff14
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-11-09 11:48:06 +0000

    Make lines extraction from input string part of initialization

--- lib/lines_counter.rb
+++ lib/lines_counter.rb
@@ -1,7 +1,8 @@
 class LinesCounter
 
   def initialize(arguments)
-    @source_code = arguments[:source_code]
+    source_code = arguments[:source_code]
+    @source_lines = source_code.split("\n")
     @one_line_regex = arguments[:one_line_regex] || JAVA_ONE_LINE_COMMENT_REGEX
   end
 
@@ -10,16 +11,15 @@ class LinesCounter
   end
 
   def lines_of_code
-    lines = source_code.split("\n")
-    return 0 if lines.empty?
-    no_empty_lines = remove_empty_lines(lines)
+    return 0 if source_lines.empty?
+    no_empty_lines = remove_empty_lines(source_lines)
     no_one_liners = remove_one_line_comments(no_empty_lines)
     no_one_liners.count
   end
 
   private
   
-  attr_reader :source_code, :one_line_regex
+  attr_reader :source_lines, :one_line_regex
 
   def remove_empty_lines(lines)
     lines.reject { |line| line =~ EMPTY_LINE_REGEX }
