642ec7b5edadc796b0df08766ae21ecdac4e647f
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-11-09 11:30:30 +0000

    Add + implement first test with one-line comment

--- lib/lines_counter.rb
+++ lib/lines_counter.rb
@@ -1,7 +1,14 @@
 class LinesCounter
 
   def self.lines_of_code(source_code)
-    source_code.length == 0 ? 0 : 1
+    lines = source_code.split("\n")
+    return 0 if lines.empty?
+    lines_without_one_liners = remove_one_line_comments(lines)
+    lines_without_one_liners.count
+  end
+
+  def self.remove_one_line_comments(lines)
+    lines.reject { |line| line =~ /^\/\//}
   end
 
 end
--- spec/unit/lines_counter_spec.rb
+++ spec/unit/lines_counter_spec.rb
@@ -4,8 +4,17 @@ describe LinesCounter, '#lines_of_code' do
     expect(described_class.lines_of_code("")).to eq 0
   end
 
-  it 'returns 1 for a string with a single line' do
+  it 'returns 1 for a string with a single line that is not a comment' do
     expect(described_class.lines_of_code("private int number = 3")).to eq 1
+  end
+
+#At this point, I start thinking that it would make a lot of sense to use start/end
+#comment anchors, should I let this shape future tests? or should future tests 
+#make this emerge? Maybe I should pretend the Client is only asking for one language
+#but keep an eye open for potential new languages...
+
+  it 'returns 0 for a string with a single line being a comment (java)' do
+    expect(described_class.lines_of_code("//private int number = 3")).to eq 0
   end
 
 end
