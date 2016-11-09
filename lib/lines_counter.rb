cbf427e88e3ae3963d49d60d48d6218129ffce2c
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-11-09 11:34:59 +0000

    Refactor to make Java comment regex a default that can be overridden

--- lib/lines_counter.rb
+++ lib/lines_counter.rb
@@ -1,14 +1,29 @@
 class LinesCounter
 
+  def initialize(arguments)
+    @source_code = arguments[:source_code]
+    @one_line_regex = arguments[:one_line_regex] || JAVA_ONE_LINE_COMMENT_REGEX
+  end
+
   def self.lines_of_code(source_code)
+    new(source_code: source_code).lines_of_code
+  end
+
+  def lines_of_code
     lines = source_code.split("\n")
     return 0 if lines.empty?
     lines_without_one_liners = remove_one_line_comments(lines)
     lines_without_one_liners.count
   end
 
-  def self.remove_one_line_comments(lines)
-    lines.reject { |line| line =~ /^\/\//}
+  private
+  
+  attr_reader :source_code
+
+  def remove_one_line_comments(lines)
+    lines.reject { |line| line =~ JAVA_ONE_LINE_COMMENT_REGEX }
   end
+
+  JAVA_ONE_LINE_COMMENT_REGEX = /^\/\//
 
 end
